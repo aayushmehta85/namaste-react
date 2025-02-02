6b11670ba94f56cda2d88f7ce3602961a566aa4d
aayush.mehta <aayush.mehta@tothenew.com>
2025-02-02 17:49:19 +0530

    episode3: Laying the foundation

--- App.js
+++ App.js
@@ -1,17 +1,21 @@
-// const heading = React.createElement('h1',{id:'heading'},"Welcome to the world of React");
-// const root = ReactDOM.createRoot(document.getElementById("root"))
-// root.render(heading)
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 
-const parent = React.createElement("div", { id: "parent" }, [
-    React.createElement('div', { id: "child1" }, [
-        React.createElement("h1", {}, "This is heading h1"), 
-        React.createElement("h2", {}, "This is heading h2")
-    ]),
-    React.createElement('div', { id: "child2" }, [
-        React.createElement("h1", {}, "This is heading h1"), 
-        React.createElement("h2", {}, "This is heading h2")
-    ])
-])
+const Title = ()=> (
+    <h1 className="heading" tabIndex="5">
+        Namaste React using JSX
+    </h1>
+)
 
-const root = ReactDOM.createRoot(document.getElementById("root"))
-root.render(parent)
+const HeadingComponent = () => (
+    <div className="container">
+        <Title/>
+        <h1 className="heading">
+            Namaste React Functional Component
+        </h1>
+    </div>
+)
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+
+root.render(<HeadingComponent/>)
